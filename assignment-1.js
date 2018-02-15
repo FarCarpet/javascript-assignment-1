176484d3998fa4fdf92f856b0aa7b63e982ed58f
Sam <35470380+FarCarpet@users.noreply.github.com>
2018-02-15 12:03:01 -0700

    assignment and helpful files

---
+++ assignment-1.js
@@ -0,0 +1,8 @@
+// 
+//
+// var provArray = ["Alberta", "British Colombia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan"];
+//
+//
+// function loadProvinces(){
+//
+// }
